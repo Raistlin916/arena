198e3330413ba86f277523e3c9b2948d33e46fa9
unknown <>
2016-03-05 17:24:34 +0800

    add webpack and babel

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './app';
+
+
+ReactDom.render(<App/>, document.getElementById('container'));
