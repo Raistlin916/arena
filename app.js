38c8362b17b538ed942750662de68111ccdeef99
unknown <>
2016-03-05 14:41:07 +0800

    initial routes

--- app.js
+++ app.js
@@ -2,19 +2,18 @@
 
 const Koa = require('koa');
 const logger = require('koa-logger');
-const router = require('koa-router')();
 const favicon = require('koa-favicon');
+const Jade = require('koa-jade');
+const jade = new Jade({
+  viewPath: './views'
+});
 
 const app = new Koa();
+const router = require('./routes');
+jade.use(app);
 
 app.use(logger())
   .use(favicon())
   .use(router.routes())
   .use(router.allowedMethods())
-  .listen(3000);
-
-
-router
-  .get('/', async (ctx) => {
-    ctx.body = 'Hello World2!';
-  });
+  .listen(3000, () => console.log('server listening on port 3000'));
