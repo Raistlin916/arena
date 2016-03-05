4b2cf363090e84c2875c0d826c068247b353263e
unknown <>
2016-03-05 16:05:48 +0800

    add static file

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const Koa = require('koa');
 const logger = require('koa-logger');
 const favicon = require('koa-favicon');
 const Jade = require('koa-jade');
-const jade = new Jade({
-  viewPath: './views'
-});
+const send = require('koa-send');
+const path = require('path');
 
 const app = new Koa();
 const router = require('./routes');
+const jade = new Jade({
+  viewPath: './views'
+});
 jade.use(app);
+
 
 app.use(logger())
   .use(favicon())
   .use(router.routes())
   .use(router.allowedMethods())
+  .use(async (ctx) => {
+  	return send(ctx, ctx.path, { root: path.join(__dirname, '/static') });
+	})
   .listen(3000, () => console.log('server listening on port 3000'));
