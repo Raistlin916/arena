dc56a0eccf50949002560b21addb1d9ade07685c
unknown <>
2016-03-13 22:17:48 +0800

    init base with redux

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDom from 'react-dom';
-import App from './app';
+import { createStore } from 'redux';
+import App from './containers/App';
+import reducers from './reducers';
 
+const store = createStore(reducers, {
+	visibility: true
+});
 
-ReactDom.render(<App/>, document.getElementById('container'));
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('container')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ module.exports = {
           exclude: /(node_modules|bower_components)/,
           loader: 'babel',
           query: {
-            presets: ['es2015', 'react', 'stage-0']
+            presets: ['es2015', 'react', 'stage-0'],
+            plugins: ["transform-decorators-legacy"]
           }
         }
       ]
