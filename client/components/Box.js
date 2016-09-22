3c56a7a3fff697f41565ff21e84eea1ec3ea448b
Raistlin916 <raistlin916@gmail.com>
2016-09-22 20:07:25 +0800

    add fsm and tween

--- client/components/Box.js
+++ client/components/Box.js
@@ -3,12 +3,15 @@ import Entity from '../proton/Entity'
 export default class Box extends Entity {
   render(ctx) {
     ctx.save()
+    super.render(ctx)
+
     ctx.translate(this.coord.x, this.coord.y)
     ctx.rotate(this.angle)
     ctx.fillStyle = '#FAE766'
     ctx.lineWidth = 2
     const x = -this.width / 2
     const y = -this.height / 2
+    ctx.scale(this.scale, this.scale)
     ctx.fillRect(x, y, this.width, this.height)
     ctx.strokeStyle = '#8B8C8B'
     ctx.strokeRect(x, y, this.width, this.height)
--- client/proton/Entity.js
+++ client/proton/Entity.js
@@ -1,15 +1,94 @@
 import CoreEntity from '../../server/proton/Entity'
 import Interpolation from './Interpolation'
 
-export default class Entity extends CoreEntity {
-  constructor(...args) {
-    super(...args)
-    this.interpolation = new Interpolation(this)
-    this.dying = false
+class FSM {
+  constructor(model, fsmMap, initialState) {
+    this.model = model
+    this.read(model, fsmMap)
+    this.status = initialState
+  }
+
+  read(model, fsmMap) {
+    Object.keys(fsmMap).forEach((key) => {
+      this[key] = () => {
+        model[`on${key.charAt(0).toUpperCase() + key.slice(1)}`]()
+        this.status = fsmMap[key].to
+      }
+    })
+  }
+
+  isAt(status) {
+    return this.status === status
+  }
+}
+
+class Tweens {
+  constructor() {
+    this.map = {}
+  }
+
+  add(name, duration, cb) {
+    let elapse = 0
+    let percent = 0
+    duration /= 1000
+    this.map[name] = {
+      update: (dt) => {
+        if (percent >= 1) {
+          return delete this.map[name]
+        }
+        elapse += dt
+        percent = Math.min(elapse / duration, 1)
+        return percent
+      },
+      render: ctx => cb(ctx, percent)
+    }
   }
 
   update(dt) {
-    if (this.dying) {
+    Object.keys(this.map).forEach((key) => {
+      this.map[key].update(dt)
+    })
+  }
+
+  render(ctx) {
+    Object.keys(this.map).forEach((key) => {
+      this.map[key].render(ctx)
+    })
+  }
+}
+
+export default class Entity extends CoreEntity {
+  constructor(...args) {
+    super(...args)
+
+    this.scale = 1
+    this.interpolation = new Interpolation(this)
+
+    this.fsm = new FSM(this, {
+      injured: { from: 'idle', to: 'idle' },
+      die: { from: 'idle', to: 'dead' }
+    }, 'idle')
+
+    this.tweens = new Tweens()
+  }
+
+  onInjured() {
+    this.tweens.add('injured', 1000, (ctx, percent) => {
+      // ctx.scale(percent, percent)
+      console.log(percent)
+    })
+  }
+
+  onDie() {
+    this.tweens.add('injured', 100, (ctx, percent) => {
+      this.scale = 1 + 1 * percent
+    })
+  }
+
+  update(dt) {
+    this.tweens.update(dt)
+
+    if (this.fsm.isAt('dead')) {
       super.update(dt)
       return
     }
@@ -17,11 +96,15 @@ export default class Entity extends CoreEntity {
     this.interpolation.update(dt)
   }
 
+  render(ctx) {
+    this.tweens.render(ctx)
+  }
+
   die() {
-    if (this.dying) {
+    if (this.fsm.isAt('dead')) {
       return
     }
-    this.dying = true
+    this.fsm.die()
     this.addInterval(() => super.die(), 1000)
   }
 }
